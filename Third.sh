758f4db82b72a50ae2e698a893fa880f87ff04a7
Iamneo <techadmin@iamneo.ai>
2023-09-13 06:56:02 +0000

    Backup Commit

--- Third.sh
+++ Third.sh
@@ -4,4 +4,4 @@ touch my_file1
 chmod 644 my_folder
 chmod 744 my_file1
 cd /home/coder/project/workspace
-echo "The new folder and file creation and their "
+echo "The new folder and file creation and their modifications are done."
