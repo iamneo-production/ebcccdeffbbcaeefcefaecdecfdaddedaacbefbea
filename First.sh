d786bf64e9a2a87b4a521710de935441b0052859
Iamneo <techadmin@iamneo.ai>
2023-09-13 07:01:02 +0000

    Backup Commit

---
+++ First.sh
@@ -0,0 +1,5 @@
+cd /home/coder
+mkdir new_folder
+touch new_file.txt
+cd /home/coder/project/workspace
+echo "New folder and file are created in the home directory"
--- Third.sh
+++ Third.sh
@@ -1,7 +1,7 @@
 cd /home/coder
 mkdir my_folder
-touch my_file1
+touch my_file1.txt
 chmod 644 my_folder
-chmod 744 my_file1
+chmod 744 my_file1.txt
 cd /home/coder/project/workspace
 echo "The new folder and file creation and their modifications are done."
