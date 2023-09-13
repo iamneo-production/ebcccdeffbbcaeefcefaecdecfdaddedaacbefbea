7e6c979c5ab044205119db0d1372ac46857ee908
Iamneo <techadmin@iamneo.ai>
2023-09-13 07:11:02 +0000

    Backup Commit

--- Fourth.sh
+++ Fourth.sh
@@ -1,2 +1,4 @@
-read -p "Enter the date(DDdate month year
-echo "$date-$month-$year"
+read -p "Enter the date(DD): " date
+read -p "Enter the month(MM): " month
+read -p "Enter the year(YYYY): " year
+echo "Your Date of Birth is: $date-$month-$year"
