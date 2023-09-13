af93c6b07cbdae78822bff2fc5618e0edf6e9c2e
Iamneo <techadmin@iamneo.ai>
2023-09-13 07:21:10 +0000

    Backup Commit

--- Fifth.sh
+++ Fifth.sh
@@ -1,11 +1,8 @@
 read -p "Enter the first input: " num1
 read -p "Enter the second input: " num2
-if [[ $[num1+num2] > "70" ]];
+if [[ $[num1+num2] -gt "70" ]];
 then
 echo "Pass"
-elif [[ $[num1+num2] < "70"]]
-then
-echo "Fail"
 else
-echo "Equals to 70"
-fi;
+echo "Fail"
+fi
